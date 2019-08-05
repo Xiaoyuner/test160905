24fc22624bd92472d74b0c28f322deb33dbeef1c
zhouyun <zhouyun@vcfilm.cn>
2019-08-05 17:09:28 +0800

    子模块git命令测试

--- src/js/index.js
+++
@@ -1,5 +0,0 @@
-var swiper=require("../../bower_components\Swiper\dist\js\swiper.js");
-           require("../../bower_components\Swiper\dist\css\swiper.css");
-    var designWidth=320;
-    var radio=document.documentElement.clientWidth/designWidth*100;
-    document.documentElement.style.fontSize=radio+"px";
